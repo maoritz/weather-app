520310f03e31d799c9b2a1480da3c324a71a5975
Maor Attias <maorattias17@gmail.com>
2024-08-30 03:51:49 +0300

    set up to app. Install libraries:Tailwind React-router Axios

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='min-h-screen justify-center text-center bg-gray-700'>
+         
+      </div>
+
+    </>
+  )
+}
+
+export default App
