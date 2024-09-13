b1aee4dbb3a500f3dbbbc7e20d28cf9ee325c9d6
Maor Attias <maorattias17@gmail.com>
2024-09-13 16:23:35 -0700

    searchbar color change.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ function SearchBar({ value, onChange, suggestions, onSelectSuggestion }) {
     <div className="relative w-full">
       <input
         type="text"
-        className="w-full text-sm text-gray-200 opacity-80 py-2 pl-5 pr-4 rounded-2xl shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none bg-gray-600"
+        className="w-full border border-gray-500 text-sm text-gray-600 opacity-90 py-2 pl-5 pr-4 rounded-2xl shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none bg-white"
         placeholder="Search weather location..."
         value={value}
         onChange={onChange}
@@ -13,7 +13,7 @@ function SearchBar({ value, onChange, suggestions, onSelectSuggestion }) {
 
       {/* Suggestions Dropdown */}
       {suggestions.length > 0 && (
-        <ul className="text-gray-300 absolute opacity-90 bg-gray-600 w-full mt-2 rounded-lg shadow-lg max-h-60 overflow-auto z-10">
+        <ul className="text-gray-600 absolute opacity-95 bg-white w-full mt-2 rounded-lg shadow-lg max-h-60 overflow-auto z-10">
           {suggestions.map((suggestion, index) => (
             <li
               key={index}
